18d50925a8bca279345ebb7779358e8b60c07b7c
v74c63t <vanesst5@uci.edu>
2023-02-16 23:59:54 -0800

    Added in checking the kb of index on disk

--- report.py
+++ report.py
@@ -1,7 +1,9 @@
-
+import json
+import sys
 def report(num_of_docs):
-
-    index = #json load the index from file/disk
+    #json load the index from file/disk
+    with open('index.json') as file:
+        index = json.load(file)
     # check how many keys are in teh dictionary
     # that should be the amount of unique words
     num_of_unique = len(index.keys())
@@ -9,3 +11,6 @@ def report(num_of_docs):
         f.write(f'Number of documents: {num_of_docs}\n')
         f.write(f'Number of unique words: {num_of_unique}')
         # figure out how to get size of index on disk
+        bytes = sys.getsizeof(index)
+        kb = bytes/1000
+        f.write(f'Total size of index on disk: {kb} KB')
