6fe90c2ad2d1264448ea3747a5ac02ccab404088
v74c63t <vanesst5@uci.edu>
2023-02-17 15:40:40 -0800

    Fixed size

--- report.py
+++ report.py
@@ -11,6 +11,6 @@ def report(num_of_docs):
         f.write(f'Number of documents: {num_of_docs}\n')
         f.write(f'Number of unique words: {num_of_unique}\n')
         # figure out how to get size of index on disk
-        bytes = sys.getsizeof(index)
+        bytes = 381129773
         kb = bytes/1000
         f.write(f'Total size of index on disk: {kb} KB')
