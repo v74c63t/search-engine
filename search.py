161723e4d664b8cf787465070749ffc05671bdd8
v74c63t <vanesst5@uci.edu>
2023-02-23 00:43:19 -0800

    Split query and got their postings

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import index
-
+import nltk
 
 def search():
     file_path = Path('./index.json')
@@ -9,4 +9,12 @@ def search():
         index.build_index(documents)
         index.tfidf(len(documents))
     query = input()
+    #queries = []
+    stemmer = nltk.PorterStemmer
+    postings = []
+    for q in query.split():
+        #queries.append(stemmer.stem(q))
+        postings.append(index[stemmer.stem(q)])
+    
+
     # figure out what to do with index
