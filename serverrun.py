a94cfb1c914a14cf2d4d01d526348167108348a7
bigliam78 <78501204+bigliam78@users.noreply.github.com>
2023-03-09 16:28:32 -0800

    just changed importing method

--- serverrun.py
+++ serverrun.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 import webbrowser
-from search import load_index, search
+import search
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.jinja_env.auto_reload = True
 
-index_pos, N, documents = load_index()
+index_pos, N, documents = search.load_index()
 
 #the web gui only has one page, and it automatically creates a numbered list upon the urllist var being filled.
 @app.route("/", methods=['POST', 'GET'])
@@ -14,7 +14,7 @@ def index():
 	nquery = request.form["input"]
 	if request.method == "POST" and nquery:
 		#functions inside search.py need to be changed to support a single call
-		urllist = search(documents, index_pos, N, 5, nquery)
+		urllist = search.search(documents, index_pos, N, 5, nquery)
 		return render_template('base.html', queries = "Displaying results for: " + nquery, urllist = urllist)
 	else:
 		return render_template('base.html')
